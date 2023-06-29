5129e4fbf116b6c8abd4f413108b2b04de0229a9
Lauren Porter <137825320+LaurenUSF@users.noreply.github.com>
2023-06-29 19:02:34 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace deliverable1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            int input = 0;
+            string series = "";
+            while (true)
+            {
+                Console.Write("enter an integer between 1 and 100: ");
+                input = int.Parse(Console.ReadLine());
+                if (input > 0 && input <= 100)
+               
+                {
+                    break;
+                }
+            }
+
+            while (true)
+            {
+                Console.WriteLine("Specify the series type: Even or Odd");
+                series = Console.ReadLine();
+
+                if (series == "Even" || series == "Odd")
+                {
+                    break;
+
+                }
+            }
+
+            if (series == "Odd")
+            {
+                Console.WriteLine("You have selected " + series + "series. The numbers between 0 and " + input + " are ");
+                for (int i = 1; i <= input; i = i + 2)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            if (series != "Even")
+            {
+                return;
+            }
+            Console.WriteLine("You have selected " + series + "series. The numbers between 0 and " + input + "are");
+            for (int i = 0; i <= input; i = i + 2)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        catch
+        {
+            Console.WriteLine("Invalid inputs");
+        }
+    }
+
+
+}
